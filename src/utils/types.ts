b0f06b13aaf88fb9fe31550017fe9df8e1f82890
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2025-06-06 20:30:37 +0530

    chore: remove incorrect versionNumber type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -31,7 +31,6 @@ export type OnError = (error: Error) => void;
 
 export type ProofRequestOptions = {
   log?: boolean;
-  versionNumber?: string;
   acceptAiProviders?: boolean;
   useAppClip?: boolean;
   device?: string;
