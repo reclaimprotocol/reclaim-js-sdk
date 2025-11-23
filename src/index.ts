e37d67ee8264c2ac11695cb172def65714b1b9f6
kryptocodes <srivatsantb@gmail.com>
2025-11-23 20:51:00 +0530

    fix: deprecate timeStamp - naming convention
    - add timestamp field to ProofProperiesJSON type
    - add timestamp&timeStamp to 'toJsonString' for backward compatibility
    
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './Reclaim';
 export * from './utils/interfaces';
-export { ClaimCreationType, ModalOptions, DeviceType } from './utils/types';
+export { ClaimCreationType, ModalOptions, DeviceType, ProofPropertiesJSON } from './utils/types';
 // Export device detection utilities for debugging (optional)
 export { 
     getDeviceType, 
