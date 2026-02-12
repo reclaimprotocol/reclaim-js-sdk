ee13825d9a7e802c41a28d01891c36c39e4e2fc7
Mushaheed Syed <mushaheed@reclaimprotocol.org>
2026-02-12 16:54:55 +0530

    Refactor isHttpResponseStatusRetryable for simple status code check
    Move response status guard before error

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,44 +2,15 @@ import fetchRetry from "fetch-retry";
 
 const MAX_RETRIES = 3;
 
-const _defaultRetryableStatuses = [
-    408, // Request Timeout
-    429, // Too Many Requests
-    500, // Internal Server Error
-    502, // Bad Gateway
-    503, // Service Unavailable
-    504, // Gateway Timeout
-
-    // From IIS
-    440, // Login Timeout
-
-    // From Ngnix
-    499, // Client Closed Request
-
-    // From AWS Elastic Load Balancer
-    460, // Client Closed Request
-
-    // Not in RFC:
-    598, // Network Read Timeout Error
-    599, // Network Connect Timeout Error
-
-    // Cloudflare Statuses
-    520, // Web Server Returned Unknown Error
-    521, // Web Server Is Down
-    522, // Connection Timed Out
-    523, // Origin Is Unreachable
-    524, // Timeout Occurred
-    525, // SSL Handshake Failed
-    527, // Railgun Error,
-];
-
-const isHttpResponseStatusRetryable = (statusCode: number) => _defaultRetryableStatuses.includes(statusCode);
+const isHttpResponseStatusRetryable = (statusCode: number) => {
+    return statusCode === 408 || statusCode === 429 || statusCode >= 500;
+};
 
 export const http = {
     get client() {
         return fetchRetry(globalThis.fetch, {
             retries: MAX_RETRIES,
-            retryDelay: function (attempt, error, response) {
+            retryDelay: function (attempt, _, response) {
                 if (response) {
                     const retryAfter = response.headers.get('Retry-After');
                     if (retryAfter) {
@@ -69,18 +40,12 @@ export const http = {
                 if (attempt >= MAX_RETRIES) {
                     return false;
                 }
-                if (error) {
-                    if (error.name === 'AbortError') {
-                        return false;
-                    }
-                    return true;
-                }
 
-                if (response) {
+                if (response && Number.isInteger(response.status)) {
                     return isHttpResponseStatusRetryable(response.status);
                 }
 
-                return false;
+                return !!error && error.name !== 'AbortError';
             },
         })
     },
