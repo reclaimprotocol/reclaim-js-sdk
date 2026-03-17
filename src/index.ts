2db9ba71e21bbb042b754dececada4edc2bfcd1e
Mushaheed Syed <mushaheed@reclaimprotocol.org>
2026-03-17 07:31:43 +0530

    Update index to include utils export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 export * from './Reclaim';
 export type * from './utils/interfaces';
 export type * from './utils/types';
+export * from './utils/proofUtils';
+export type * from './utils/proofUtils';
+export * from './utils/proofValidationUtils';
+export type * from './utils/proofValidationUtils';
+export * from './utils/providerUtils';
+export type * from './utils/providerUtils';
+export * from './utils/sessionUtils';
+export type * from './utils/sessionUtils';
 // Export device detection utilities for debugging (optional)
 export {
     getDeviceType,
